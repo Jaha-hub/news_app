d0a5a583ac36d6d544532b6b64ddc3e81ce376d5
Jaha-hub <nuratdinovzahangir4@gmail.com>
2026-01-08 20:36:50 +0500

    4

--- categories/schemas.py
+++ categories/schemas.py
@@ -1,6 +1,1 @@
 from pydantic import Field, EmailStr, field_validator, model_validator, BaseModel
-
-class UserBase(BaseModel):
-    title: str = Field(min_length=3, max_length=320)
-    result = ''.join(char for char in title if not char.isdigit())
-    print(result)
