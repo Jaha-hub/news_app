441a1f68e90981cd97b9089d066dca20e6b0b7ef
Jaha-hub <nuratdinovzahangir4@gmail.com>
2026-01-17 13:38:39 +0500

    7

--- article/models.py
+++ article/models.py
@@ -10,7 +10,7 @@ class Article(Base, IntIDMixin, TimeActionMixin):
 
     title = Column(String(512), nullable=False)
     content = Column(Text,nullable=False)
-    category_id = Column(Integer, ForeignKey('categories.id', ondelete="SET NULL"), nullable=False)
-    author_id = Column(Integer, ForeignKey('users.id', ondelete="SET NULL"), nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.id', ondelete="SET NULL"))
+    author_id = Column(Integer, ForeignKey('users.id', ondelete="SET NULL"))
     status = Column(String(20), nullable=False, default='draft')
-    published_at = Column(DateTime, nullable=False)
+    published_at = Column(DateTime)
